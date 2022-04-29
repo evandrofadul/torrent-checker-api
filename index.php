310981b6679d7861de1d2c8bf23c3fc39acb2d15
evandro <evandrofadul@gmail.com>
2022-04-29 14:56:52 -0300

    -

--- index.php
+++ index.php
@@ -1,44 +1,46 @@
-<?php require_once('./engine/torrent.php'); require_once('./engine/scraper.php');
-    if (isset($_GET['torrent'])) {
-        echo header('Content-type: json');
-        preg_match('/h:(.*?)&/', ($_GET['torrent']), $hashInfo);
-        preg_match_all('/&tr=(.*?)&/', urldecode($_GET['torrent']), $magnetInfo);
+<?php
+require_once('./engine/torrent.php');
+require_once('./engine/scraper.php');
 
-        $torrent = new Torrent("http://itorrents.org/torrent/{$hashInfo[1]}.torrent");
+if (isset($_GET['torrent'])) {
+    echo header('Content-type: json');
+    preg_match('/h:(.*?)&/', ($_GET['torrent']), $hashInfo);
+    preg_match_all('/&tr=(.*?)&/', urldecode($_GET['torrent']), $magnetInfo);
 
-        if ($torrent->size() != null) {
-            $torrentFile ='';
-            foreach ($torrent->content() as $key => $item) {
-                $file .= str_replace("{$torrent->name()}/", '', $key) . '*';
-            }
-            $torrentFiles = array_filter(explode('*', $file));
-            $torrentData = [
-                'hash' => $hashInfo[1],
-                'nome' => $torrent->name(),
-                'criado' => date('d-m-Y H:i:s', $torrent->creation_date()),
-                'tamanho' => $torrent->size(2),
-                'arquivos' => $torrentFiles,
-                'trackers' => $magnetInfo[1]
-            ];
+    $torrent = new Torrent("http://itorrents.org/torrent/{$hashInfo[1]}.torrent");
 
-            $scraper = new Scrapeer\Scraper();
-            $tracker = $magnetInfo[1];
-            $hash = $hashInfo[1];
-            $info = $scraper->scrape($hash, $tracker);
-            $torrentScrape = [
-                'seeds' => $info[$hash]['seeders'],
-                'peers' => $info[$hash]['leechers'],
-                'completed' => $info[$hash]['completed']
-            ];
-            $torrentApi = array_merge($torrentData, $torrentScrape);
-
-            echo json_encode($torrentApi, JSON_UNESCAPED_UNICODE);
-            
-        } else {
-            echo json_encode(['error' => 'torrent not found']);
+    if ($torrent->size() != null) {
+        $torrentFile ='';
+        foreach ($torrent->content() as $key => $item) {
+            $file .= str_replace("{$torrent->name()}/", '', $key) . '*';
         }
-        return http_response_code(200);
-    } else 
+        $torrentFiles = array_filter(explode('*', $file));
+        $torrentData = [
+            'hash' => $hashInfo[1],
+            'nome' => $torrent->name(),
+            'criado' => date('d-m-Y H:i:s', $torrent->creation_date()),
+            'tamanho' => $torrent->size(2),
+            'arquivos' => $torrentFiles,
+            'trackers' => $magnetInfo[1]
+        ];
+
+        $scraper = new Scrapeer\Scraper();
+        $tracker = $magnetInfo[1];
+        $hash = $hashInfo[1];
+        $info = $scraper->scrape($hash, $tracker);
+        $torrentScrape = [
+            'seeds' => $info[$hash]['seeders'],
+            'peers' => $info[$hash]['leechers'],
+            'completed' => $info[$hash]['completed']
+        ];
+        $torrentApi = array_merge($torrentData, $torrentScrape);
+
+        echo json_encode($torrentApi, JSON_UNESCAPED_UNICODE);
+        return http_response_code(200); 
+    }
+    echo json_encode(['error' => 'torrent not found']);
+    return http_response_code(404);
+} else
 ?>
 <!DOCTYPE html>
 <head>
@@ -97,26 +99,26 @@
 
         if (isset($data['error'])) {
             echo $data['error'];
-        } else {
-            $torrentHtml = '<form method="get"><button type="submit" name="torrent" value="'. $_POST['torrent'] .'">GET API (Json)</button></form>';
-
-            $torrentHtml .= "<br>Nome: <b>{$data['nome']}</b>";
-            $torrentHtml .= "<br>Informações do Hash: <b>{$data['hash']}</b>";
-            $torrentHtml .= "<br>Tamanho: <b>{$data['tamanho']}</b>";
-            $torrentHtml .= "<br>Criado em: <b>{$data['criado']}</b>";
-            $torrentHtml .= "<br>Downloads Completos: <b>{$data['completed']}</b>";
-            $torrentHtml .= "<br>Seeds: <b>{$data['seeds']}</b>";
-            $torrentHtml .= "<br>Peers: <b>{$data['peers']}</b>
-                            <br>Arquivos:";
-            foreach ($data['arquivos'] as $item) {
-                $torrentHtml .= "<br/>&emsp;<b>$item</b>";
-            }
-            $torrentHtml .= '<br>Trackers:';
-            foreach ($data['trackers'] as $item) {
-                $torrentHtml .= "<br>&emsp;<b>$item</b>";
-            }
-            echo $torrentHtml;
+            return http_response_code(404);
         }
+        $torrentHtml = '<form method="get"><button type="submit" name="torrent" value="'. $_POST['torrent'] .'">GET API (Json)</button></form>';
+
+        $torrentHtml .= "<br>Nome: <b>{$data['nome']}</b>";
+        $torrentHtml .= "<br>Informações do Hash: <b>{$data['hash']}</b>";
+        $torrentHtml .= "<br>Tamanho: <b>{$data['tamanho']}</b>";
+        $torrentHtml .= "<br>Criado em: <b>{$data['criado']}</b>";
+        $torrentHtml .= "<br>Downloads Completos: <b>{$data['completed']}</b>";
+        $torrentHtml .= "<br>Seeds: <b>{$data['seeds']}</b>";
+        $torrentHtml .= "<br>Peers: <b>{$data['peers']}</b>
+                            <br>Arquivos:";
+        foreach ($data['arquivos'] as $item) {
+            $torrentHtml .= "<br/>&emsp;<b>$item</b>";
+        }
+        $torrentHtml .= '<br>Trackers:';
+        foreach ($data['trackers'] as $item) {
+            $torrentHtml .= "<br>&emsp;<b>$item</b>";
+        }
+        echo $torrentHtml;
     }
     ?>
     <div class="footer">
